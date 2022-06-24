10f38ab3346f5831b63b0bdfc67b636a1e05ff49
frozein <59415054+frozein@users.noreply.github.com>
2022-06-24 16:48:20 -0400

    basic opengl setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,52 @@
+#define GLFW_DLL
+
 #include <iostream>
+#include <GLAD/glad.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	std::cout << "hello\n";
+	//init GLFW:
+	//---------------------------------
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//create and init window:
+	//---------------------------------
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "DoonUI", NULL, NULL);
+	if(window == NULL)
+	{
+		printf("Failed to create GLFW window\n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	//load opengl functions:
+	//---------------------------------
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		printf("Failed to initialize GLAD\n");
+		return -1;
+	}
+
+	//set gl viewport:
+	//---------------------------------
+	glViewport(0, 0, 1280, 720);
+
+	//main loop:
+	//---------------------------------
+	while(!glfwWindowShouldClose(window))
+	{
+		//finish rendering and swap:
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	//clean up and close:
+	//---------------------------------
+	glfwTerminate();
+	return 0;
 }
